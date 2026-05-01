26893b550b823a3a5b94d8699efab92ff0778d3e
bufansong2019 <bufansong2019@163.com>
2026-05-01 21:09:26 +0800

    新增Beaudar插件用作单留言板

---
+++ docs/.vuepress/components/Beaudar.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="beaudar-comments">
+    <script src="https://beaudar.lipk.org/client.js"
+            repo="bufansong2019/bufansong2019.github.io"
+            branch="main"
+            issue-term="留言板"
+            theme="github-light"
+            comment-order="desc"
+            input-position="top"
+            crossorigin="anonymous"
+            async>
+    </script>
+  </div>
+</template>
